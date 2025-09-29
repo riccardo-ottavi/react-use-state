6eb4cb287ea3a2379a878596d1d15449c4b5b269
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-29 14:43:44 +0200

    inserisci la card per la descrizione del linguaggio

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import MyBtnList from './components/MyBtnList'
 import MyHeader from "./components/MyHeader"
+import LanguageCard from './components/LanguageCard'
 
 function App() {
   console.log("provaaaaaaaaaa")
@@ -9,6 +10,7 @@ function App() {
     <>
       <MyHeader/>
       <MyBtnList/>
+      <LanguageCard/>
     </>
   )
 }
